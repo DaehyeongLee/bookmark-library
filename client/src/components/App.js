673774bca70930b9145d9841731477300470a3bb
daelee <apm5018@naver.com>
2021-07-09 10:40:21 +0900

    Landing page template

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
       <NavBar />
-      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+      <div className = "app" style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {Button, Input, Form} from 'antd';
 
 function LandingPage() {
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         
-            <div>
-                
+            <div className = "app">
+                <Form onSubmit = {onSubmit}>
+                        <Form.Item
+                            label="Search the bookmarks you want"
+                            name="Bookmark_Search"
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Button type="primary" className="bookmark_search_btn" onClick = {onSubmit}>Search</Button>
+                    </Form>
             </div>
         
     )
