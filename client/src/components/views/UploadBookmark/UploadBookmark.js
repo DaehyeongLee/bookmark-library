3c62a19c3e490c0400b8dfc0464fffd02744ca0d
daelee <apm5018@naver.com>
2021-08-06 18:34:53 +0900

    Upload Bookmark Page template - 1

--- client/src/components/views/UploadBookmark/UploadBookmark.js
+++ client/src/components/views/UploadBookmark/UploadBookmark.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {useSelector} from 'react-redux';
+import SelectedTitle from '../Commons/SelectedTitle';
+import { useSelector } from 'react-redux';
 //mport { Container, Row, Col } from 'reactstrap';
-import {Collapse, Button, Input, Row, Col, Form} from 'antd';
+import { Collapse, Button, Input, Row, Col, Form } from 'antd';
 import './Sections/UploadBookmark.css';
+import '../Commons/body_design.css';
 const { Panel } = Collapse;
 const { TextArea } = Input;
 
@@ -19,7 +21,7 @@ function Bookmark() {
     const [bookmarkDescription, setbookmarkDescription] = useState("")
 
     //console.log(result)
-    
+
 
     useEffect(() => {
         axios.post('/api/bookmark/getUsername').then(response => {
@@ -28,7 +30,7 @@ function Bookmark() {
                 setwinUsername(response.data.username)
 
                 const path = `C:/Users/${response.data.username}/AppData/Local/Google/Chrome/User Data/Default/Bookmarks`
-                axios.post('/api/bookmark/readBookmark', {path: path}).then(response => {
+                axios.post('/api/bookmark/readBookmark', { path: path }).then(response => {
                     if (response.data.success) {
                         //console.log(JSON.parse(response.data.bookmark))
                         setbookmarkData(JSON.parse(response.data.bookmark))
@@ -64,73 +66,77 @@ function Bookmark() {
         }
 
         axios.post('/api/bookmark/uploadBookmark', variables)
-        .then(response => {
-            if(response.data.success) {
+            .then(response => {
+                if (response.data.success) {
 
-                alert('성공적으로 업로드 되었습니다.')
-                
+                    alert('성공적으로 업로드 되었습니다.')
 
-            } else {
-                alert('북마크 업로드에 실패했습니다.')
-            }
-        })
+
+                } else {
+                    alert('북마크 업로드에 실패했습니다.')
+                }
+            })
     }
 
     return (
-        <div className = "bookmark_body">
-
-            <h4>My chrome bookmark list :</h4> <br />
-            
-                <Row>
-                <Col className = "bookmark_panel">
-                    {bookmarkData && bookmarkData.roots.bookmark_bar.children.map((item, index) => {
-                        if (item.type == "folder") {
-                            return <React.Fragment key={index}><Collapse>
-                                <Panel header={item.name} >
-                                    {item && item.children.map((childItem, childIndex) => {
-                                        return <div key={childIndex}>{childIndex + 1}.&nbsp;&nbsp;&nbsp;{childItem.name}</div>
-                                    })
-                                    }
-                                    <Button className="bookmark_select_btn" onClick = {() => onSelectBookmark(item.children)}>Select</Button>
-                                </Panel>
-                            </Collapse>
-                            </React.Fragment>
+        <React.Fragment>
+            <div style={{ width: '100%', margin: '0', padding: '0' }}>
+                <SelectedTitle fieldName="Upload" />
+            </div>
+            <div className="Result-body">
+                <Row gutter={16}>
+                    <Col className="panel" span={12}>
+                        <h4>My chrome bookmark list :</h4> <br />
+                        {bookmarkData && bookmarkData.roots.bookmark_bar.children.map((item, index) => {
+                            if (item.type == "folder") {
+                                return <React.Fragment key={index}>
+                                    <Collapse>
+                                        <Panel header={item.name} >
+                                            {item && item.children.map((childItem, childIndex) => {
+                                                return <div key={childIndex}>{childIndex + 1}.&nbsp;&nbsp;&nbsp;{childItem.name}</div>
+                                            })
+                                            }
+                                            <div className="upload_btn"><Button type="primary" onClick={() => onSelectBookmark(item.children)}>Select</Button></div>
+                                        </Panel>
+                                    </Collapse>
+                                </React.Fragment>
+                            }
+                        })
                         }
-                    })
-                    }
-                </Col>
-                <Col className="bookmark_selectedPanel">
-                    {isSelectedBookmark &&
-                    <Form onSubmit = {onSubmit}>
-                        <Form.Item
-                            label="Bookmark Detail List"
-                            name="Bookmark Detail List"
-                        >
-                            {selectedBookmark && isSelectedBookmark && selectedBookmark.map((item, index) => {
-                                return <div key={index}>{index + 1}.&nbsp;&nbsp;&nbsp;<a href={item.url} target="_blank">{item.name}</a></div>
-                            })}
-                        </Form.Item>
-                        <Form.Item
-                            label="Title"
-                            name="Title"
-                            rules={[{ required: true, message: 'Please input title' }]}
-                        >
-                            <Input onChange={onTitleChange} value={bookmarkTitle}/>
-                        </Form.Item>
-                        <Form.Item
-                            label="Description"
-                            name="Description"
-                            rules={[{ required: true, message: 'Please input description' }]}
-                        >
-                            <TextArea onChange={onDescriptionChange} value={bookmarkDescription}/>
-                        </Form.Item>
-                        <Button type="primary" className="bookmark_select_btn" onClick = {onSubmit}>Upload</Button>
-                    </Form>
-                    }
-                </Col>
-            </Row>
+                    </Col>
+                    <Col className="selected" span={12}>
+                        {isSelectedBookmark &&
+                            <Form onSubmit={onSubmit}>
+                                <Form.Item
+                                    label="Bookmark Detail List"
+                                    name="Bookmark Detail List"
+                                >
+                                    {selectedBookmark && isSelectedBookmark && selectedBookmark.map((item, index) => {
+                                        return <div key={index}>{index + 1}.&nbsp;&nbsp;&nbsp;<a href={item.url} target="_blank">{item.name}</a></div>
+                                    })}
+                                </Form.Item>
+                                <Form.Item
+                                    label="Title"
+                                    name="Title"
+                                    rules={[{ required: true, message: 'Please input title' }]}
+                                >
+                                    <Input onChange={onTitleChange} value={bookmarkTitle} />
+                                </Form.Item>
+                                <Form.Item
+                                    label="Description"
+                                    name="Description"
+                                    rules={[{ required: true, message: 'Please input description' }]}
+                                >
+                                    <TextArea onChange={onDescriptionChange} value={bookmarkDescription} />
+                                </Form.Item>
+                                <div className="upload_btn"><Button type="primary" onClick={onSubmit}>Upload</Button></div>
+                            </Form>
+                        }
+                    </Col>
+                </Row>
 
-        </div>
+            </div>
+        </React.Fragment>
     )
 }
 
